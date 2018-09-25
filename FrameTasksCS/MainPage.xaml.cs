71d9b124b28688a97050aed03f37247453bae966
Victor Isaev <visaev@saber3d.ru>
2018-09-25 23:13:10 +0200

    VM with timer displaying

--- FrameTasksCS/MainPage.xaml.cs
+++ FrameTasksCS/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Threading;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,14 +19,42 @@ using Windows.UI.Xaml.Navigation;
 
 namespace FrameTasksCS
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
+
+
+
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+
+            m_TasksVM = new TasksVM();
+
+            this.DataContext = m_TasksVM;
+
+            // set initial data (through binding)
+            m_T1Value = 0;
+            m_TasksVM.T1 = "my task!!";
+
+            ThreadPoolTimer PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer((ThreadPoolTimer timer) => 
+            {
+                m_T1Value += 1;
+                Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+                {
+                    m_TasksVM.T1 = m_T1Value.ToString();
+                });
+            }, TimeSpan.FromMilliseconds(450));
         }
+
+        
+        // threads
+
+        // values
+        int m_T1Value;
+
+        // VM
+        TasksVM m_TasksVM;
     }
 }
---
+++ FrameTasksCS/TasksVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameTasksCS
+{
+    public class TasksVM : INotifyPropertyChanged
+    {
+        public string T1
+        {
+            get { return m_T1; }
+            set { m_T1 = value; OnPropertyChanged("T1"); }  // the name is used in XAML
+        }
+        private string m_T1;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(String info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
